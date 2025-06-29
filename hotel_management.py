35d3cdfb0e3c022feaeddf2c1448de9b330e1439
SRohini01 <srohini0801@gmail.com>
2025-06-29 15:22:45 +0530

    Add files via upload

---
+++ hotel_management.py
@@ -0,0 +1,115 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================================= RESTART: C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY ================================================
+						 WELCOME TO HOTEL ANCASA
+
+			 1 Booking
+
+			 2 Rooms Info
+
+			 3 Room Service(Menu Card)
+
+			 4 Payment
+
+			 5 Record
+
+			 0 Exit
+
+->1
+ 
+ BOOKING ROOMS
+ 
+Name: rohini
+Phone No.: 9110362185
+Address: alwal
+Check-In: 2/8/26
+Invalid date
+
+Traceback (most recent call last):
+  File "C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY", line 533, in <module>
+    Home()
+  File "C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY", line 37, in Home
+    Booking()
+  File "C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY", line 162, in Booking
+    date(ci)
+  File "C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY", line 125, in date
+    phno.pop(i)
+IndexError: pop from empty list
+>>> 
+================================================= RESTART: C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY ================================================
+						 WELCOME TO HOTEL ANCASA
+
+			 1 Booking
+
+			 2 Rooms Info
+
+			 3 Room Service(Menu Card)
+
+			 4 Payment
+
+			 5 Record
+
+			 0 Exit
+
+->1
+ 
+ BOOKING ROOMS
+ 
+Name: rohini
+Phone No.: 9110362185
+Address: alwal
+Check-In: 2/8/2026
+Invalid date
+
+Traceback (most recent call last):
+  File "C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY", line 533, in <module>
+    Home()
+  File "C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY", line 37, in Home
+    Booking()
+  File "C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY", line 162, in Booking
+    date(ci)
+  File "C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY", line 125, in date
+    phno.pop(i)
+IndexError: pop from empty list
+
+================================================= RESTART: C:\Users\ROHINI\AppData\Local\Programs\Python\Python312\DSP MICRO PROJECT.PY ================================================
+						 WELCOME TO HOTEL ANCASA
+
+			 1 Booking
+
+			 2 Rooms Info
+
+			 3 Room Service(Menu Card)
+
+			 4 Payment
+
+			 5 Record
+
+			 0 Exit
+
+->1
+ 
+ BOOKING ROOMS
+ 
+Name: rohini
+Phone No.: 9110362185
+Address: alwal
+Check-In: 2/8/2025
+Check-Out: 5/8/2025
+----SELECT ROOM TYPE----
+ 1. Standard Non-AC
+ 2. Standard AC
+ 3. 3-Bed Non-AC
+ 4. 3-Bed AC
+		Press 0 for Room Prices
+->4
+Room Type- 3-Bed AC
+Price- 5000
+
+			***ROOM BOOKED SUCCESSFULLY***
+
+Room No. -  309
+Customer Id -  23
+0-BACK
+ ->3
